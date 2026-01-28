51c8f37cbe19689d539f20828f3b8fcb4681a4e1
11divyansh <bhatt11divyansh@gmail.com>
2026-01-28 16:34:34 +0530

    test(prompts): add basic rendering test for PromptTemplate

---
+++ src/test/java/io/oxyjen/llm/prompts/PromptTest.java
@@ -0,0 +1,34 @@
+package io.oxyjen.llm.prompts;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class PromptTest {
+	
+	private void log(String title) {
+        System.out.println("\n==============================");
+        System.out.println(title);
+        System.out.println("==============================");
+    }
+
+    private void print(String label, Object value) {
+        System.out.println(label + " => " + value);
+    }
+    @Test
+    void testBasicRendering() {
+    	log("Testing basic rendering");
+        PromptTemplate template = PromptTemplate.of(
+            "Hello {{name}}, you are {{age}} years old"
+        );
+        
+        String result = template.render(
+            "name", "Alice",
+            "age", 25
+        );
+        print("result",result);
+        
+        assertEquals("Hello Alice, you are 25 years old", result);
+    }
+
+}
