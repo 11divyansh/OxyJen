0823cbee0c5e01689c4c01d1578b5c067cbb3c6d
11divyansh <bhatt11divyansh@gmail.com>
2026-03-11 14:44:50 +0530

    tests(schemanode): add SchemaNode tests

--- src/test/java/io/oxyjen/llm/schema/tests/SchemaNodeTests.java
+++ src/test/java/io/oxyjen/llm/schema/tests/SchemaNodeTests.java
@@ -2,12 +2,16 @@ package io.oxyjen.llm.schema.tests;
 
 import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
 import io.oxyjen.core.NodeContext;
+import io.oxyjen.llm.ChatModel;
 import io.oxyjen.llm.schema.JSONSchema;
 import io.oxyjen.llm.schema.JSONSchema.PropertySchema;
 import io.oxyjen.llm.schema.SchemaNode;
@@ -83,5 +87,108 @@ public class SchemaNodeTests {
 	    Tags t = node.process("input", new NodeContext());
 	    out.println(t);
 	    assertEquals(2, t.tags().size());
+	}
+//	@Test
+//	void schemaNode_handlesCollection() {
+//		log("SchemaNode handles collections");
+//	    
+//	    JSONSchema schema = JSONSchema.object()
+//	        .property("tags",
+//	            JSONSchema.PropertySchema.array(
+//	                JSONSchema.PropertySchema.string("tag")
+//	            ))
+//	        .required("tags")
+//	        .build();
+//	    SchemaNode<List> node =
+//	        SchemaNode.builder(List.class)
+//	            .model(new FakeModel("{\"tags\":[\"a\",\"b\"]}"))
+//	            .schema(schema)
+//	            .build();
+//
+//	    List<String> t = node.process("input", new NodeContext());
+//	    out.println(t);
+//	    //assertEquals(2, t.tags().size());
+//	}
+	@Test
+	void schemaNode_handlesOptional() {
+		log("Schema node handles optional");
+	    record User(String name, Optional<String> nickname){}
+	    JSONSchema schema = JSONSchema.object()
+	        .property("name", JSONSchema.PropertySchema.string("name"))
+	        .property("nickname", JSONSchema.PropertySchema.string("nick"))
+	        .required("name")
+	        .build();
+	    SchemaNode<User> node =
+	        SchemaNode.builder(User.class)
+	            .model(new NewFakeModel("{\"name\":\"John\"}"))
+	            .schema(schema)
+	            .build();
+	    User u = node.process("input", new NodeContext());
+	    out.println(u);
+	    assertTrue(u.nickname().isEmpty());
+	}
+	
+	@Test
+	void schemaNode_handlesEnum() {
+		log("Schema node handles enum");
+		enum Priority{low, high}
+		record Ticket(Priority priority) {}
+		JSONSchema schema = JSONSchema.object()
+				.property("priority", PropertySchema.enumOf("p","low","high"))
+				.required("priority")
+				.build();
+		SchemaNode<Ticket> node = SchemaNode.builder(Ticket.class)
+				.model(new NewFakeModel("{\"priority\":\"low\"}"))
+				.schema(schema)
+				.build();
+		Ticket t = node.process("input", new NodeContext());
+		out.println(t);
+		assertEquals(Priority.low, t.priority());
+	}
+	
+	@Test
+	void schemaNode_retriesUntilValid() {
+		log("Schemanode retries until valid");
+		record Ticket(String name) {}
+		ChatModel model = new ChatModel() {
+			int call = 0 ;
+			@Override
+			public String chat(String input) {
+				call++;
+				return call == 4 ? "{\"name\":\"ok\"}" : "{}";
+			}
+		};
+		JSONSchema schema = JSONSchema.object()
+				.property("name", PropertySchema.string("name"))
+				.required("name")
+				.build();
+		SchemaNode<Ticket> node = SchemaNode.builder(Ticket.class)
+				.model(model)
+				.schema(schema)
+				.maxRetries(4)
+				.build();
+		Ticket t = node.process("input", new NodeContext());
+		out.println(t);
+		assertEquals("ok",t.name());
+	}
+	@Test
+	void schemaNode_storesMemory() {
+		log("SchemaNode stores memory");
+	    record Ticket(String name){}
+	    NodeContext ctx = new NodeContext();
+	    JSONSchema schema = JSONSchema.object()
+	        .property("name", JSONSchema.PropertySchema.string("name"))
+	        .required("name")
+	        .build();
+
+	    SchemaNode<Ticket> node =
+	        SchemaNode.<Ticket>builder(Ticket.class)
+	            .model(new FakeModel("{\"name\":\"John\"}"))
+	            .schema(schema)
+	            .memory("test")
+	            .build();
+	    node.process("hello", ctx);
+	    out.println(ctx.memory("test").entries());
+	    assertFalse(ctx.memory("test").entries().isEmpty());
 	}
 }
