8495d7634ca7cc482f2d8faadbd7fb701e6816c6
11divyansh <bhatt11divyansh@gmail.com>
2026-03-28 22:21:07 +0530

    feat(core): enhance GraphBuilder with addNode(name, node) to resgister nodes with name, and connect(from, to) to connect nodes just by their names. Node names provide clean graph definition instead of passing objects everywhere

--- src/main/java/io/oxyjen/core/GraphBuilder.java
+++ src/main/java/io/oxyjen/core/GraphBuilder.java
@@ -1,7 +1,9 @@
 package io.oxyjen.core;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -11,7 +13,8 @@ import java.util.Objects;
 public class GraphBuilder {
 
     private String name = "unnamed-graph";
-    private final List<NodePlugin<?, ?>> nodes = new ArrayList<>();
+    Map<String, NodePlugin<?, ?>> nodes = new LinkedHashMap<>();
+    private final List<Edge> edges = new ArrayList<>();
 
     public static GraphBuilder named(String name) {
         GraphBuilder builder = new GraphBuilder();
@@ -19,17 +22,44 @@ public class GraphBuilder {
         return builder;
     }
 
-    public GraphBuilder addNode(NodePlugin<?, ?> node) {
+    // Node registration with name
+    public GraphBuilder addNode(String name, NodePlugin<?, ?> node) {
+        Objects.requireNonNull(name, "Node name cannot be null");
         Objects.requireNonNull(node, "Node cannot be null");
-        nodes.add(node);
+        if (nodes.containsKey(name)) {
+            throw new IllegalArgumentException(
+                "Duplicate node name: " + name
+            );
+        }
+        nodes.put(name, node);
+        return this;
+    }
+    /**
+     * Simple direct connection (A -> B)
+     */
+    public GraphBuilder connect(String from, String to) {
+        NodePlugin<?, ?> source = getNode(from);
+        NodePlugin<?, ?> target = getNode(to);
+        edges.add(new DirectEdge(source, target));
         return this;
     }
 
     public Graph build() {
         Graph graph = new Graph(name);
-        for (NodePlugin<?, ?> node : nodes) {
+        for (NodePlugin<?, ?> node : nodes.values()) {
             graph.addNode(node);
         }
+        for (Edge edge : edges) {
+        	graph.addEdge(edge);
+        }
+        // validation
         return graph;
+    }
+    private NodePlugin<?, ?> getNode(String name){
+    	NodePlugin<?, ?> node = nodes.get(name);
+    	if (node == null) {
+    		throw new IllegalArgumentException("Node not found: " + name);
+    	}
+    	return node;
     }
 }
