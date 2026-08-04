a9e51705e9372b77f93ac3169a9b048961560752
11divyansh <bhatt11divyansh@gmail.com>
2026-08-04 21:42:24 +0530

    feat(observe): introduce ExecutionLogger

---
+++ src/main/java/io/oxyjen/observe/ExecutionLogger.java
@@ -0,0 +1,15 @@
+package io.oxyjen.observe;
+
+import java.util.logging.Logger;
+
+import io.oxyjen.execution.ExecutionEvent;
+
+public final class ExecutionLogger implements ObservationListener {
+
+	private static final Logger LOG = Logger.getLogger("io.oxyjen");
+
+	@Override
+	public void onEvent(ExecutionEvent event) {		
+
+	}
+}
